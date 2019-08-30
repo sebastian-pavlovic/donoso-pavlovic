cadc71f41cc65d8dcc4de8ca1c3aaa663cd9bcd8
EtnecivOsonod <53793148+EtnecivOsonod@users.noreply.github.com>
2019-08-30 16:10:59 -0400

    Add files via upload

--- portafolio.js
+++ portafolio.js
@@ -50,3 +50,14 @@ $.each(trabajos, function(i, val) {
     $('#portafolio').append('<div class="item">' + uno + dos + tres + cuatro + '</div>')
 });
 
+window.onscroll = function() {myFunction()};
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+    if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky")
+    } else{
+        navbar.classList.remove("sticky");
+    }
+}
